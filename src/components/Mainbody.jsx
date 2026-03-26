95f45ce58393a900d7f0b1caf7ca7671bd26ecfc
Pachecolosgroup <cesarppublicidad03@gmail.com>
2026-03-26 07:53:06 -0500

    newComponents
    structuring the whole html, separate the most important parts in each component, and add them to the App.jsx

---
+++ src/components/Mainbody.jsx
@@ -0,0 +1,15 @@
+import Guitar from "./Guitar";
+
+function Mainbody() {
+  return (
+    <main className="container-xl mt-5">
+      <h2 className="text-center">Nuestra Colección</h2>
+
+      <div className="row mt-5">
+        <Guitar />
+      </div>
+    </main>
+  );
+}
+
+export default Mainbody;
