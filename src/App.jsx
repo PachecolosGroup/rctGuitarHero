fafec6795c8776ae983accd10b4cd45e194da59c
Pachecolosgroup <cesarppublicidad03@gmail.com>
2026-04-05 12:48:45 -0500

    DataBase
    Data base, useState, reading and adding props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./index.css";
+import React, { useState } from "react";
 import Header from "./components/Header";
-import Mainbody from "./components/Mainbody";
 import Footer from "./components/Footer";
+import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setdata] = useState(db);
   return (
     <>
       <Header />
-      <Mainbody />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Coleccion</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} />
+          ))}
+        </div>
+      </main>
+
       <Footer />
     </>
   );
