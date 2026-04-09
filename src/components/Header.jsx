c4f831fbc3cdbcc9ef10c96034d1c703237098b6
Pachecolosgroup <cesarppublicidad03@gmail.com>
2026-04-09 14:29:23 -0500

    itemsOnSite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React, { useMemo } from "react";
 
-function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity }) {
+function Header({
+  cart,
+  removeFromCart,
+  increaseQuantity,
+  decreaseQuantity,
+  clearCart,
+}) {
   // State deruvadi
   const isEmpty = useMemo(() => cart.length === 0, [cart]);
   const cartTotal = useMemo(
@@ -96,7 +102,10 @@ function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity }) {
                   </>
                 )}
 
-                <button className="btn btn-dark w-100 mt-3 p-2">
+                <button
+                  className="btn btn-dark w-100 mt-3 p-2"
+                  onClick={clearCart}
+                >
                   Vaciar Carrito
                 </button>
               </div>
