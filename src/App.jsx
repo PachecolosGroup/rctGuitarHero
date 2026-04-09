79e7523c61aa9262c978ac8bab0e6696ab7407ac
Pachecolosgroup <cesarppublicidad03@gmail.com>
2026-04-09 12:50:40 -0500

    idGuitar Changes
    Changes and more changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,34 @@ import { db } from "./data/db";
 
 function App() {
   const [data, setdata] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
   return (
     <>
-      <Header />
+      <Header cart={cart} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Coleccion</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
