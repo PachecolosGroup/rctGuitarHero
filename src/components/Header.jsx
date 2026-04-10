41c786f708970b2b797e09685f56ea470acbda6b
Pachecolosgroup <cesarppublicidad03@gmail.com>
2026-04-10 11:53:47 -0500

    customHooks
    Eliminar la logica de los templates, para separarlos y poderlo reutilizar de una manera mas comoda

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,12 @@
-import React, { useMemo } from "react";
-
 function Header({
   cart,
   removeFromCart,
   increaseQuantity,
   decreaseQuantity,
   clearCart,
+  isEmpty,
+  cartTotal,
 }) {
-  // State deruvadi
-  const isEmpty = useMemo(() => cart.length === 0, [cart]);
-  const cartTotal = useMemo(
-    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
-    [cart],
-  );
-
   return (
     <header className="py-5 header">
       <div className="container-xl">
